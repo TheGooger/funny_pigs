06fbae89b59e21d9ce202368a3cde2d8091b0198
Vladimir <vladimir.ivanov1307@gmail.com>
2022-06-01 13:04:40 +0300

    New screen height

--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ class Settings:
         """Initialization of game settings."""
         # Screen parameters.
         self.screen_width = 1200
-        self.screen_height = 750
+        self.screen_height = 700
         self.bg_color = (0, 150, 0)
